aea31cf9ac2c3450226da31900076bc2f697c3a0
Lihui <lihui.han.13@gmail.com>
2024-10-12 23:56:47 +0800

    setup mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const profileRoutes = require('./routes/profile')
 const app = express()
 const port = 3000
-
-// listen for requests
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+const mongo_uri = "mongodb+srv://tic3901:2401NWRG1@tic3901.omjhh.mongodb.net/?retryWrites=true&w=majority&appName=TIC3901";
 
 // middleware
 app.use(express.json())
@@ -15,4 +12,17 @@ app.use((req, res, next) => {
     next()
 })
 
+// routes
 app.use('/profiles', profileRoutes)
+
+// connect to mongodb
+mongoose.connect(mongo_uri)
+    .then(() => {
+        // listen for requests
+        app.listen(port, () => {
+            console.log(`Connected to DB and listening on port ${port}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
